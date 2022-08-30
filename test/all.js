68fe35ff28224818a6e2835a74bf94b1263eafa4
nazeh <ar.nazeh@gmail.com>
2022-08-30 09:10:49 +0300

    fix: typo

--- test/all.js
+++ test/all.js
@@ -4,7 +4,7 @@ import createTestnet from '@hyperswarm/testnet'
 
 import * as auth from '../index.js'
 
-test('auth', async t => {
+test('authz', async t => {
   const testnet = await createTestnet(3, t.teardown)
 
   const alice = new Slashtag(testnet)
@@ -18,7 +18,7 @@ test('auth', async t => {
     .slice(2, 6)
 
   const server = new auth.Server(alice, {
-    onauth: (_token, remote) => {
+    onauthz: (_token, remote) => {
       st.is(_token, token)
       st.is(remote.id, bob.id)
       return true
@@ -50,14 +50,14 @@ test('magic link', async t => {
 
   const magiclink = 'https://www.example.com/user?q=one-time-pass'
 
-  new auth.Server(alice, {
+  const server = new auth.Server(alice, {
     onmagiclink: remote => {
       st.is(remote.id, bob.id)
       return magiclink
     }
   })
 
-  await alice.listen()
+  await server.slashtag.listen()
 
   const client = new auth.Client(bob)
   const response = await client.magiclink(alice.url)
@@ -74,16 +74,31 @@ test('missing callback', async t => {
   const alice = new Slashtag(testnet)
   const bob = new Slashtag(testnet)
 
-  new auth.Server(alice)
+  const server = new auth.Server(alice, {})
 
-  await alice.listen()
+  await server.slashtag.listen()
 
   const client = new auth.Client(bob)
 
-  await t.exception(client.authz(alice.url), 'Authz not implemented')
-  await t.exception(client.magiclink(alice.url), 'Magic link not implemented')
+  await t.exception(client.magiclink(alice.url), /Magic link not implemented/)
+  // await t.exception(client.authz(alice.url + '?q=foo'), /Authz not implemented/)
 
-  t.pass('test')
+  await alice.close()
+  await bob.close()
+})
+
+test('authz - missing token', async t => {
+  const testnet = await createTestnet(3, t.teardown)
+
+  const alice = new Slashtag(testnet)
+  const bob = new Slashtag(testnet)
+
+  const server = new auth.Server(alice)
+
+  await server.slashtag.listen()
+
+  const client = new auth.Client(bob)
+  await t.exception(client.authz(alice.url), /Missing token in slashauth url/)
 
   await alice.close()
   await bob.close()
